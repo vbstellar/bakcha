0b3160ad776ccae3e6814ef191decabe243b1744
Vishu Bansal <bansalvishu56@gmail.com>
2024-01-29 23:14:30 +0530

    Middlewares setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_Name } from "./constants";
 import connectDB from "./db/page.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", () => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb Connection failed!!: ", err);
+  });
 
 // import express from "express";
 // const app = express();
